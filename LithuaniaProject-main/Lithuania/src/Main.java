619403a0822cc613cc036d4c203a5dec02a80082
rhit-hautid <hautid@rose-hulman.edu>
2023-03-24 10:32:09 -0400

    update have image

--- LithuaniaProject-main/Lithuania/src/Main.java
+++ LithuaniaProject-main/Lithuania/src/Main.java
@@ -37,36 +37,37 @@ public class Main {
 		
 	
 		
-//		
-//		
-//		ImageIcon img = null;
-//		try {
-//			img = new ImageIcon(new URL("file:///C:/Users/hautid/git/LithuaniaProject/LithuaniaProject-main/Images/MySquare.PNG"));
-//		} catch (MalformedURLException e) {
-//			e.printStackTrace();
-//		}
-//		JLabel jlPic = new JLabel(img);
-//
-//		jlPic.setPreferredSize(new Dimension(300,100));
-//		
-//		panel.add(jlPic, BorderLayout.CENTER);
-//		
+		
+		
+		ImageIcon img = null;
+		try {
+			img = new ImageIcon(new URL("file:///C:/Users/hautid/git/LithuaniaProject/LithuaniaProject-main/Images/MySquare.PNG"));
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		JLabel jlPic = new JLabel(img);
+
+		jlPic.setPreferredSize(new Dimension(300,100));
+		Dimension size = jlPic.getPreferredSize();
+		jlPic.setBounds (1000, 300, size.width, size.height); //will let you place button anywhere you want based on coordinates 
+		panel.add(jlPic, BorderLayout.CENTER);
+		
 		
 		
 		JButton germanyButton = new JButton("GERMANY");
 		germanyButton.setPreferredSize(new Dimension(200, 100)); //sets the size of the Germany button with (width, height)
 		frame.add(panel, BorderLayout.NORTH); //adds the panel and centers it
 		germanyButton.setFont(new Font("American Typewriter", Font.BOLD, 20)); // sets the font for the Germany Button 
-		Dimension size = germanyButton.getPreferredSize();
-		germanyButton.setBounds (500, 300, size.width, size.height); //will let you place button anywhere you want based on coordinates 
+		Dimension sizeTwo = germanyButton.getPreferredSize();
+		germanyButton.setBounds (500, 300, sizeTwo.width, sizeTwo.height); //will let you place button anywhere you want based on coordinates 
 		panel.add(germanyButton); //adds the Germany button to the panel
 		
 		JButton russiaButton = new JButton("RUSSIA");
 		russiaButton.setPreferredSize(new Dimension(200, 100)); //sets the size of the Russia button with (width, height)
 		frame.add(panelTwo); //adds the panel and centers it
 		russiaButton.setFont(new Font("American Typewriter", Font.BOLD, 20)); //sets the font for the Russia Button
-		Dimension sizeTwo = russiaButton.getPreferredSize();
-		russiaButton.setBounds (1000, 300, sizeTwo.width, sizeTwo.height); //will let you place button anywhere you want based on coordinates 
+		Dimension sizeThree = russiaButton.getPreferredSize();
+		russiaButton.setBounds (1000, 300, sizeThree.width, sizeThree.height); //will let you place button anywhere you want based on coordinates 
 		panelTwo.add(russiaButton, BorderLayout.SOUTH); //adds the Russia button to the panel
 		
 		frame.setVisible(true);
