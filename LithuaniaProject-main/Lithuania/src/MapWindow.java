65e29f8b55903ed109906b53674f928afb3690f2
rhit-hautid <hautid@rose-hulman.edu>
2023-04-04 10:24:20 -0400

    buttons on map

--- LithuaniaProject-main/Lithuania/src/MapWindow.java
+++ LithuaniaProject-main/Lithuania/src/MapWindow.java
@@ -15,18 +15,41 @@ import javax.swing.JPanel;
  * @author Isabel Haut
  */
 
-public class MapWindow  {
-	
-	//Instantiated components 
+public class MapWindow {
+
+	// Instantiated components
 	JFrame mapWindow;
 	JPanel mapPanel;
-	
+	CircleButton[] circleButton = new CircleButton[10];
+
 	public MapWindow() {
-	
+
 		JFrame mapWindow = new JFrame("Map Window");
 		JPanel mapPanel = new JPanel();
 		mapPanel.setLayout(null);
 		mapWindow.add(mapPanel);
+
+		int[][] buttonCoordinates = {
+				{ 100, 210 }, // first button coordinates
+				{ 120, 120 }, // second button coordinates
+				{ 140, 140 }, // third button coordinates
+				{ 160, 160 }, // fourth button coordinates
+				{ 180, 180 }, // fifth button coordinates
+				{ 200, 200 }, // sixth button coordinates
+				{ 220, 220 }, // seventh button coordinates
+				{ 240, 240 }, // eighth button coordinates
+				{ 260, 260 }, // ninth button coordinates
+				{ 280, 280 }, // tenth button coordinates
+		};
+
+		// adds 10 of my circle buttons to the window
+		for (int i = 0; i < 10; i++) {
+			circleButton[i] = new CircleButton(null);
+			circleButton[i].setBounds(buttonCoordinates[i][0], buttonCoordinates[i][1], 20, 20);
+			mapPanel.add(circleButton[i]);
+			System.out.println(circleButton);
+			circleButton[i].repaint();
+		}
 
 		// add image of map to the panel
 		ImageIcon mySquare = new ImageIcon("Lithuania/src/Images/LithuanianMap.PNG");
@@ -34,12 +57,13 @@ public class MapWindow  {
 		Dimension size = jlPic.getPreferredSize();
 		jlPic.setBounds(0, 0, size.width, size.height);
 		mapPanel.add(jlPic);
-		
+
 		// set size and visibility of the map window
 		mapWindow.setSize(810, 700);
 		mapPanel.setBackground(Color.WHITE);
 		mapWindow.setVisible(true);
+		mapPanel.setVisible(true);
 
-		
-}
+	}
+
 }
