200b12433ffdcca04308f012c111e1b357434a52
hughessa <hughessa@rose-hulman.edu>
2023-04-17 10:07:50 -0400

    Text

---
+++ LithuaniaProject-main/Lithuania/src/ReadTextAsString.java
@@ -0,0 +1,19 @@
+
+ 
+import java.nio.file.*;
+ 
+public class ReadTextAsString {
+   
+  public static String readFileAsString(String fileName)throws Exception
+  {
+    String data = "";
+    data = new String(Files.readAllBytes(Paths.get(fileName)));
+    return data;
+  }
+ 
+  public static void main(String[] args) throws Exception
+  {
+    String data = readFileAsString("C:\\Users\\pankaj\\Desktop\\test.java");
+    System.out.println(data);
+  }
+}
