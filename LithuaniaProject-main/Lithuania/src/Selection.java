f6a3942d773c4078118dd128bafbbd4d61b41110
rhit-hautid <hautid@rose-hulman.edu>
2023-04-20 10:04:37 -0400

    Labels added to selection screen

--- LithuaniaProject-main/Lithuania/src/Selection.java
+++ LithuaniaProject-main/Lithuania/src/Selection.java
@@ -1,6 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Insets;
 import java.util.ArrayList;
 
@@ -11,78 +12,96 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
-
 /*
- * @author Sebastien Hughes
+ * @author Sebastien Hughes and Isabel Haut
  */
-public class Selection extends Abstract_DataDisplay{
-	
+public class Selection extends Abstract_DataDisplay {
+
 	public static final Dimension FIRST_SCREEN_SIZE = new Dimension(1500, 830);
-	
+
 	ArrayList<ImageButton> List_Button = new ArrayList<ImageButton>();
-	
-	
+	ArrayList<JLabel> myLabel = new ArrayList<JLabel>();
+
 	Color myBlue = new Color(245, 224, 143);
 	JFrame frame_Selection;
 	JPanel panel_Selection;
-			
+
 	public Selection() {
-		
+
 		this.frame_Selection = new JFrame("Selection Screen");
 		this.panel_Selection = new JPanel();
-		
+
 		panel_Selection.setLayout(null);
-		
+
 		frame_Selection.setSize(FIRST_SCREEN_SIZE);
 		frame_Selection.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame_Selection.add(panel_Selection, BorderLayout.CENTER);
-		
+
 		panel_Selection.setBackground(myBlue);
 		panel_Selection.setSize(FIRST_SCREEN_SIZE);
-		
+
 		AddSearches();
 		CreateImageButtons();
 		dis_Image();
+		CreateLabels();
 		dis_Caption();
-		
-		
+
 		frame_Selection.setVisible(true);
-		
+		frame_Selection.repaint();
+		frame_Selection.revalidate();
+
 	}
-	
+
 	public void AddSearches() {
-		//If statement for proximaty to current location to determine relevant number of image buttons
-		for (int i = 0; i < 6; i++) {	
-			
-		System.out.println("Gimme Doz Image" + (i+1));
-		
+		// If statement for proximaty to current location to determine relevant number
+		// of image buttons
+		for (int i = 0; i < 6; i++) {
+
+			System.out.println("Gimme Doz Image" + (i + 1));
+
 		}
 	}
 
 	public void CreateImageButtons() {
-		//If statement for proximaty to current location to determine relevant number of image buttons
-		for (int i = 0; i < 6; i++) {	
-			
-		System.out.println("Creating Image Button" + i);
-		List_Button.add(new ImageButton(frame_Selection, panel_Selection,"Lithuania/src/Images/TrakaiCastle.PNG"));
-		
+		// If statement for proximaty to current location to determine relevant number
+		// of image buttons
+		for (int i = 0; i < 6; i++) {
+
+			System.out.println("Creating Image Button" + i);
+			List_Button.add(new ImageButton(frame_Selection, panel_Selection, "Lithuania/src/Images/TrakaiCastle.PNG"));
+
+		}
+	}
+
+	public void CreateLabels() {
+		// If statement for proximaty to current location to determine relevant number
+		// of image buttons
+		for (int i = 0; i < 6; i++) {
+			// System.out.println("Creating Label " + i);
+			JLabel label = new JLabel("" + i);
+
+			myLabel.add(label);
+
 		}
 	}
 
 	@Override
 	void dis_Image() {
 		Insets insets = panel_Selection.getInsets();
-		//If statement for proximaty to current location to determine relevant number of image buttons
-		for (int i = 0; i <List_Button.size(); i++) {
+		// If statement for proximaty to current location to determine relevant number
+		// of image buttons
+		for (int i = 0; i < List_Button.size(); i++) {
 			Dimension sizeTwo = List_Button.get(i).Button.getPreferredSize();
 			System.out.println(sizeTwo);
-			System.out.println("Placing Image Button " + (i+1));
-			if(i >= 3) {
-				List_Button.get(i).Button.setBounds(100 + (470*(i-3)) + insets.left, 405 + insets.bottom, sizeTwo.width, sizeTwo.height);
+			System.out.println("Placing Image Button " + (i + 1));
+			if (i >= 3) {
+				List_Button.get(i).Button.setBounds(100 + (470 * (i - 3)) + insets.left, 405 + insets.bottom,
+						sizeTwo.width, sizeTwo.height);
 				panel_Selection.add(List_Button.get(i).Button);
-				
+
 			} else {
-				List_Button.get(i).Button.setBounds(100 + (470*i) + insets.left, 20 + insets.bottom, sizeTwo.width, sizeTwo.height);
+				List_Button.get(i).Button.setBounds(100 + (470 * i) + insets.left, 20 + insets.bottom, sizeTwo.width,
+						sizeTwo.height);
 				panel_Selection.add(List_Button.get(i).Button);
 			}
 		}
@@ -90,20 +109,34 @@ public class Selection extends Abstract_DataDisplay{
 
 	@Override
 	void dis_Caption() {
-		for (int i = 0; i < 6; i++) {	
-			
-			System.out.println("Creating Image Button" + i);
-			panel_Selection.add(new JLabel("Gimme the Juice"));
-			
+		Insets insets = panel_Selection.getInsets();
+
+		for (int i = 0; i < myLabel.size(); i++) {
+			System.out.println("Creating Label" + i);
+			myLabel.get(i).setVisible(true);
+
+			myLabel.get(i).setPreferredSize(new Dimension(100, 50));
+			Dimension sizeThree = myLabel.get(i).getPreferredSize();
+			myLabel.get(i).setFont(new Font("American Typewriter", Font.BOLD, 25));
+
+			myLabel.get(i).setText("Label " + (i + 1));
+			if (i >= 3) {
+				myLabel.get(i).setBounds(225 + (470 * (i - 3)) + insets.left, 685 + insets.bottom + sizeThree.height,
+						sizeThree.width, sizeThree.height);
+
+			} else {
+				myLabel.get(i).setBounds(225 + (470 * i) + insets.left, 345 + insets.bottom, sizeThree.width,
+						sizeThree.height);
 			}
+			panel_Selection.add(myLabel.get(i));
+			myLabel.get(i).setVisible(true);
+		}
 	}
 
 	@Override
 	void dis_HighPrio() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
-		
+
 }
